d5e688ba721476aa602befcf24f31e6daee4c38a
Lucia Cerchie <luciacerchie@gmail.com>
2020-11-23 15:37:07 -0700

    first commit

---
+++ shiba.js
@@ -0,0 +1,7 @@
+const Shiba = () => {
+    return <img src="https://d17fnq9dkz9hgj.cloudfront.net/breed-uploads/2018/08/shiba-inu-detail.jpg?bust=1535566568&width=355"></img>
+}
+
+
+ReactDOM.render(<Shiba/>, document.getElementById("root"));
+
