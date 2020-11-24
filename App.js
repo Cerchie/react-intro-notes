53de20f1332f6f72b0f97467d4c23e7ddd1afd7d
Lucia Cerchie <luciacerchie@gmail.com>
2020-11-24 09:24:51 -0700

    learning about booleans

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 const App = () => (
         <div>
+            <RandomChoice choices={['red', 'blue', 'green']} />
+            <Animal name="stevie" species="silkie" emoji=":chicken:" isCute={true}/>
+            <Animal name="foxy" species="red-tailed" emoji=":fox:" isCute={false} />
+            <Animal name="" species="" emoji="" />
             <RandomNum />
             <RandomNum />
             <RandomNum />
